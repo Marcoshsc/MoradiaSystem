3742e3360d613f184451f46b58b8b56e933dacb1
Marcoshsc <marcoshscunha@hotmail.com>
2021-08-10 11:55:57 -0300

    added places cards

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import PlacesList from "./components/PlacesList";
 import "./styles/globals.scss";
 
 function App() {
-  return <PlacesList />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/places">
+          <PlacesList />
+        </Route>
+        <Route path="/">
+          <Redirect to="/places" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- front-end/src/components/PlacesList/PlaceCard/index.tsx
+++ front-end/src/components/PlacesList/PlaceCard/index.tsx
@@ -1,8 +1,55 @@
 import React, { FC } from "react";
 import styles from "./PlaceCard.module.scss";
+import { FaBath, FaBed } from "react-icons/fa";
+import { MdPlace } from "react-icons/md";
+
+const url =
+  "https://magicfloridaimoveis.com.br/wp-content/uploads/2018/12/261694-o-que-e-melhor-alugar-ou-comprar-imovel-nos-eua-1200x800.jpg";
+const url2 = "https://www.pikpng.com/pngl/m/80-805523_default-avatar-svg-png-icon-free-download-264157.png";
 
 const PlaceCard: FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.placeImage}>
+        <img src={url} alt="Place Url" />
+      </div>
+      <div className={styles.title}>
+        <div className={styles.titleText}>
+          <p>Casa do campo</p>
+        </div>
+        <div className={styles.status}>
+          <p>VENDE-SE</p>
+        </div>
+      </div>
+      <div className={styles.roomAndUserInfo}>
+        <div className={styles.roomInfo}>
+          <div className={styles.roomInfoItem}>
+            <FaBed />
+            <p>{`4 quartos`}</p>
+          </div>
+          <div className={styles.roomInfoItem}>
+            <FaBath />
+            <p>{`5 banheiros`}</p>
+          </div>
+        </div>
+        <div className={styles.userInfo}>
+          <p>nome_usuario</p>
+          <img src={url2} alt="User avatar" />
+        </div>
+      </div>
+      <div className={styles.description}>
+        <p>
+          Lorem Ipsum simplesmente Lorem Ipsum simplesmente Lorem Ipsum simplesmente Lorem Ipsum simplesmente Lorem
+          Ipsum simplesmente Lorem Ipsum simplesmente Lorem Ipsum simplesmente Lorem Ipsum simplesmente Lorem Ipsum
+          simplesmente Lorem Ipsum simplesmente Lorem Ipsum simplesmente{" "}
+        </p>
+      </div>
+      <div className={styles.city}>
+        <MdPlace />
+        <p>Nova uniao, MG</p>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceCard;
