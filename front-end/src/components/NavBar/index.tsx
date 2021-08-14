2b1b275ffe057e7209335c64dd2983967777cf49
Raquel Martins <raquelmartins203@yahoo.com.br>
2021-08-14 16:05:01 -0300

    navbar

--- front-end/src/components/NavBar/index.tsx
+++ front-end/src/components/NavBar/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { FC } from "react";
 import styles from "./styles.module.scss";
-import Avatar from "../../images/image-login.png";
+import Avatar from "../../images/user.png";
+import MenuImg from "../../images/menu.png";
 import { useHistory } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const NavBar: FC = () => {
   const [index, setIndex] = useState(0);
   const history = useHistory();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleChangeMenu = (indexSelected: number) => {
+  const handleChangeTab = (indexSelected: number) => {
     setIndex(indexSelected);
     switch (indexSelected) {
       case 0:
@@ -23,13 +27,21 @@ const NavBar: FC = () => {
     }
   };
 
+  const handleClickMenu = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.itensWrapper}>
         <div className={styles.navitens}>
-          <div onClick={() => handleChangeMenu(0)}>imóveis</div>
-          <div onClick={() => handleChangeMenu(1)}>usuário</div>
-          <div onClick={() => handleChangeMenu(2)}>contratos</div>
+          <div onClick={() => handleChangeTab(0)}>imóveis</div>
+          <div onClick={() => handleChangeTab(1)}>usuário</div>
+          <div onClick={() => handleChangeTab(2)}>contratos</div>
         </div>
         <div
           className={styles.itemSelected}
@@ -39,7 +51,23 @@ const NavBar: FC = () => {
       <div className={styles.navuser}>
         <div className={styles.username}>Bem vindo, usuário</div>
         <img src={Avatar} className={styles.avatar}></img>
-        <img src={Avatar} className={styles.avatar}></img>
+        <img
+          src={MenuImg}
+          className={styles.menu}
+          onClick={handleClickMenu}
+        ></img>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleCloseMenu}
+        >
+          <MenuItem onClick={handleCloseMenu}>Apagar conta</MenuItem>
+          <MenuItem onClick={handleCloseMenu} className={styles.logout}>
+            Sair
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   );
