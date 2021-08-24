0014369a404335da92eee76e9ba457334a666ba7
Victor Torquato <victortorquatob@gmail.com>
2021-08-24 20:49:26 -0300

    Added bcrypto for user password and getInterest method

--- back-end/src/Controller/userController.js
+++ back-end/src/Controller/userController.js
@@ -1,15 +1,45 @@
 const prisma = require('../prisma');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
+
+async function hashPassword (password){
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(SALT_ROUNDS, (errorInSalt, salt) => {
+      if (errorInSalt) {
+        reject(errorInSalt);
+      }
+      bcrypt.hash(password, salt, (errorInHash, hash) => {
+        if (errorInHash) {
+          reject(errorInHash);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
+
+async function comparePassword(password, hash,) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
 
 module.exports = {
   async create(req, res) {
     const { name, description, email, phone, location, password } = req.body;
+    const hashedPass = await hashPassword(password);
     try {
       const data = await prisma.user.create({
         data: {
           name: name,
           description: description,
           email: email,
-          password: password,
+          password: hashedPass,
           phone: phone,
           location: location
         }
@@ -24,6 +54,21 @@ module.exports = {
   async index(req, res) {
     try {
       const data = await prisma.user.findMany();
+      res.json(data);
+    } catch (error) {
+      console.log(error.name + ":" + error.message);
+      res.status(400).send();
+    }
+  },
+
+  async getInterest(req, res) {
+    const id = req.params.id;
+    try {
+      const data = await prisma.interest.findMany({
+        where: {
+          id_user: id,
+        }
+      });
       res.json(data);
     } catch (error) {
       console.log(error.name + ":" + error.message);
@@ -63,6 +108,8 @@ module.exports = {
 
   async update(req, res) {
     const { id, newName, newDescription, newEmail, newPassword, newPhone, newLocation } = req.body;
+
+    const newPass = hashPassword(newPassword);
     try {
       const data = await prisma.user.update({
         where: {
@@ -72,7 +119,7 @@ module.exports = {
           name: newName,
           description: newDescription,
           email: newEmail,
-          password: newPassword,
+          password: newPass,
           phone: newPhone,
           location: newLocation
         }
