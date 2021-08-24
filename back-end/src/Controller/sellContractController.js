b6f4780cf657457273b9c4d90837deab27390999
Victor Torquato <victortorquatob@gmail.com>
2021-08-24 19:52:37 -0300

    Added sellContractController CRUD methods

---
+++ back-end/src/Controller/sellContractController.js
@@ -0,0 +1,66 @@
+const prisma = require('../prisma');
+
+module.exports = {
+    async create(req, res) {
+        const { value, id_place, id_user } = req.body;
+        try {
+            const data = await prisma.sellcontract.create({
+                data: {
+                    value: value,
+                    id_place: id_place,
+                    id_user: id_user
+                }
+            });
+            res.json(data);
+        } catch (error) {
+            console.log(error.name + ":" + error.message);
+            res.status(400).send();
+        }
+    },
+
+
+    async index(req, res) {
+        try {
+            const data = await prisma.sellcontract.findMany();
+            res.json(data);
+        } catch (error) {
+            console.log(error.name + ":" + error.message);
+            res.status(400).send();
+        }
+    },
+
+    async update(req, res) {
+        const { id, newValue, newId_place, newId_user } = req.body;
+        try {
+            const data = await prisma.sellcontract.update({
+                where: {
+                    id: Number(id)
+                },
+                data: {
+                    value: newValue,
+                    id_place: newId_place,
+                    id_user: newId_user
+                }
+            });
+            res.json(data);
+        } catch (error) {
+            console.log(error.name + ":" + error.message);
+            res.status(400).send();
+        }
+    },
+
+    async delete(req, res) {
+        const id = req.params.id;
+        try {
+            const data = await prisma.sellcontract.delete({
+                where: {
+                    id: Number(id)
+                },
+            });
+            res.json(data);
+        } catch (error) {
+            console.log(error.name + ":" + error.message);
+            res.status(400).send();
+        }
+    }
+}
