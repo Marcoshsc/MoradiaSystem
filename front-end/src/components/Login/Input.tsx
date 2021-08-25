a0b164d3e1dfe4503792c7ea45f4cb644993d92d
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-08-25 00:03:42 -0300

    add login

--- front-end/src/components/Login/Input.tsx
+++ front-end/src/components/Login/Input.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { FC } from "react";
 import { IconType } from "react-icons";
 import styles from "./styles.module.scss";
@@ -6,12 +7,25 @@ import styles from "./styles.module.scss";
 type InputProps = {
   Icon: IconType;
   placeholder: string;
+  callback: (input: string) => void;
 };
-const Input: FC<InputProps> = ({ Icon, placeholder }: InputProps) => {
+const Input: FC<InputProps> = ({ Icon, placeholder, callback }: InputProps) => {
+  const [value, setValue] = useState("");
+  function handleChange(input: string) {
+    setValue(input);
+  }
+
+  useEffect(() => {
+    callback(value);
+  }, [callback, value]);
   return (
     <div className={styles.inputContainer}>
       <Icon color="#217032" size={30} />
-      <input placeholder={placeholder} />
+      <input
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
--- front-end/src/components/Login/Panel.tsx
+++ front-end/src/components/Login/Panel.tsx
@@ -5,13 +5,44 @@ import Input from "./Input";
 import { ReactComponent as Logo } from "../../images/logo.svg";
 import { FaUserAlt } from "react-icons/fa";
 import { BsFillLockFill } from "react-icons/bs";
+import { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContenxt";
+import { useHistory } from "react-router-dom";
 const Panel: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const { singIn } = useContext(AuthContext);
+  const { push } = useHistory();
+  function handleEmail(input: string) {
+    setEmail(input);
+  }
+
+  function handlePassword(input: string) {
+    setPassword(input);
+  }
+
+  function handleSingIn() {
+    singIn(email, password).then((e) => {
+      if (!e) setError(!e);
+      else push("/places");
+    });
+  }
+
   return (
     <div className={styles.panel}>
       <Logo />
-      <Input placeholder="Usuário" Icon={FaUserAlt} />
-      <Input placeholder="Senha" Icon={BsFillLockFill} />
-      <button className={styles.buttonAccess}>Acessar</button>
+      <Input callback={handleEmail} placeholder="Usuário" Icon={FaUserAlt} />
+      <Input
+        callback={handlePassword}
+        placeholder="Senha"
+        Icon={BsFillLockFill}
+      />
+      <button className={styles.buttonAccess} onClick={handleSingIn}>
+        Acessar
+      </button>
+      {error && <p>Login inválido</p>}
       <p>
         Naõ possui conta? <span>Registre-se agora</span>
       </p>
